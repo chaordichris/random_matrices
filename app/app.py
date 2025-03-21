d0f919fbeb46e8ecccaee603387f8988f28298ed
cp29analytics <chris.patrick29@gmail.com>
2025-03-21 10:25:19 -0400

    reduce # of tickers

--- app/app.py
+++ app/app.py
@@ -142,25 +142,7 @@ with tab3:
     sp500_top50 = [
     "MMM", "AOS", "ABT", "ABBV", "ACN", "ATVI", "AYI", "ADBE", "AAP", "AMD",
     "AES", "AFL", "A", "APD", "AKAM", "ALK", "ALB", "ARE", "ALGN", "ALLE",
-    "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR", "AEE", "AAL", "AEP",
-    "AXP", "AIG", "AMT", "AWK", "AMP", "ABC", "AME", "AMGN", "APH", "ADI",
-    "ANSS", "AON", "APA", "AAPL", "AMAT", "APTV", "ANET", "AJG", "AIZ", "T",
-    "ATO", "ADSK", "AZO", "AVB", "AVY", "BKR", "BALL", "BAC", "BBWI", "BAX",
-    "BDX", "WRB", "BRK.B", "BBY", "BIO", "TECH", "BIIB", "BLK", "BK", "BA",
-    "BKNG", "BWA", "BXP", "BSX", "BMY", "AVGO", "BR", "BRO", "BF.B", "CHRW",
-    "CDNS", "CZR", "CPT", "CPB", "COF", "CAH", "KMX", "CCL", "CARR", "CTLT",
-    "CAT", "CBOE", "CBRE", "CDW", "CE", "CNC", "CNP", "CDAY", "CERN", "CF",
-    "CRL", "SCHW", "CHTR", "CVX", "CMG", "CB", "CHD", "CI", "CINF", "CTAS",
-    "CSCO", "C", "CFG", "CTXS", "CLX", "CME", "CMS", "KO", "CTSH", "CL",
-    "CMCSA", "CMA", "CAG", "COP", "ED", "STZ", "CEG", "GLW", "CTVA", "COST",
-    "CTRA", "CCI", "CSX", "CMI", "CVS", "DHI", "DHR", "DRI", "DVA", "DE",
-    "DAL", "XRAY", "DVN", "DXCM", "FANG", "DLR", "DFS", "DISCA", "DISCK",
-    "DISH", "DIS", "DG", "DLTR", "D", "DPZ", "DOV", "DOW", "DTE", "DUK",
-    "DRE", "DD", "DXC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA",
-    "EMR", "ENPH", "ETR", "EOG", "EPAM", "EFX", "EQIX", "EQR", "ESS", "EL",
-    "ETSY", "RE", "EVRG", "ES", "EXC", "EXPE", "EXPD", "EXR", "XOM", "FFIV",
-    "FDS", "FAST", "FRT", "FDX", "FITB", "FRC", "FE", "FIS", "FISV", "FLT",
-    "FMC", "F", "FTNT", "FTV", "FBHS", "FOXA", "FOX", "BEN", "FCX", "GRMN",
+    "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR", "AEE", "AAL", "AEP"
 ]
     # Ticker selection from sp500_top50
     # Add a "Select All" checkbox
